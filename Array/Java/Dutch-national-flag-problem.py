0db24a0d9278fcbe4e6a376b217c1e01b0cad88c
MiheretuTesh <se.miheretu.degebassa@gmail.com>
2023-11-14 12:34:08 +0300

    feat: solution in python

---
+++ Array/Java/Dutch-national-flag-problem.py
@@ -0,0 +1,21 @@
+def sortcolors(arr, size):
+    low = 0
+    high = size - 1
+    mid = 0
+
+    while(mid<=high):
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            mid += 1
+            low += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+    return arr
+
+arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+arr_size = len(arr)
+arr = sortcolors(arr, arr_size)
+print(arr)
