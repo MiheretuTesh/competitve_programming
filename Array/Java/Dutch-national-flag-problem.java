6475bce3b9947641dcdaaf998dca6459f7b7eea7
MiheretuTesh <se.miheretu.degebassa@gmail.com>
2023-11-14 12:15:16 +0300

    feat: Computitive study journey

---
+++ Array/Java/Dutch-national-flag-problem.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+// Java program to sort an array of 0, 1 and 2
+
+// Given N balls of colour red, white or blue arranged in a line in random order. You have to arrange all the balls such that the balls with the same colours are adjacent with the order of the balls, with the order of the colours being red, white and blue (i.e., all red coloured balls come first then the white coloured balls and then the blue coloured balls). 
+
+class countzot {
+    static void sortcolors(int a[], int arr_size) {
+        int low = 0;
+        int high = arr_size - 1;
+        int mid = 0, temp = 0;
+
+        while (high <= mid) {
+            switch (a[mid]) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
