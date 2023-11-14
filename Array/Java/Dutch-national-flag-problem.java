b1108b62b94cf2517ffd5e04c1cc27d19b7cc036
MiheretuTesh <se.miheretu.degebassa@gmail.com>
2023-11-14 12:22:40 +0300

    feat: Dutch National Flag problem

--- Array/Java/Dutch-national-flag-problem.java
+++ Array/Java/Dutch-national-flag-problem.java
@@ -5,19 +5,53 @@ import java.io.*;
 // Given N balls of colour red, white or blue arranged in a line in random order. You have to arrange all the balls such that the balls with the same colours are adjacent with the order of the balls, with the order of the colours being red, white and blue (i.e., all red coloured balls come first then the white coloured balls and then the blue coloured balls). 
 
 class countzot {
-    static void sortcolors(int a[], int arr_size) {
+    static int[] sortcolors(int a[], int arr_size) {
         int low = 0;
         int high = arr_size - 1;
         int mid = 0, temp = 0;
 
-        while (high <= mid) {
+        while (mid <= high) {
             switch (a[mid]) {
-
+                case 0: {
+                    temp = a[low];
+                    a[low] = a[mid];
+                    a[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1: {
+                    mid++;
+                    break;
+                }
+                case 2: {
+                    temp = a[mid];
+                    a[mid] = a[high];
+                    a[high] = temp;
+                    high--;
+                    break;
+                }
             }
         }
+
+        for (int i = 0; i < arr_size; i++) {
+            System.out.println(a[i]);
+        }
+
+        return a;
+    }
+
+    static void printArray(int arr[], int arr_size) {
+        int i;
+        for (i = 0; i < arr_size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
+        int arr_size = arr.length;
+        int[] value = sortcolors(arr, arr_size);
+        printArray(value, arr_size);
     }
 }
